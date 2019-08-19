fd8a3fab53a91ab6b6b7f9d77781b395d77aed1c
keithMaybe10 <keithmaybe10@gmail.com>
2019-08-19 19:38:58 +0800

    add mnist_train.py to train mnist model

---
+++ dl/mnist/mnist_train.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import os
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+#加载mnist_inference.py中定义的常量和前向传播的函数
+import mnist_inference
+
+#配置神经网络的参数
+BATCH_SIZE = 100
+LEARNING_RATE_DECAY = 0.99
+REGULARAZTION_RATE = 0.0001
+TRAINING_STEPS = 30000
+MOVING_AVERAGE_DECAY = 0.99
+#模型保存的路径和文件名
+MODEL_SAVE_PATH = "/path/to/model"
+MODEL_NAME = "model.ckpt"
+
+def train(mnist):
+    #定义输入输出placeholder
+    x = tf.placeholder(tf.float32,[None, mnist_inference.INPUT_NODE], name='x-input')
+    y_ = tf.placeholder(tf.float32, [None, mnist_inference.OUTPUT_NODE], name='y-input')
+    regularizer = tf.contrib.layers.l2_regularizer(REGULARAZTION_RATE)
+    #直接使用mnist_inference.py中定义的前向传播过程
+    y = mnist_inference.inference(x, regularizer)
+    global_step = tf.Variable(0, trainable=False)
+
+    #定义训练模型的损失函数、学习率、滑动平均操作及训练过程
+    variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
+    variable_averages_op = variable_averages.apply(tf.trainable_variables())
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(y_, 1))
+    cross_entropy_mean = tf.reduce_mean(cross_entropy)
+    loss = cross_entropy_mean + tf.add_n(tf.get_collection('losses'))
+    learning_rate = tf.tf.train.exponential_decay(LEARNING_RATE_DECAY, global_step, mnist.train.num_examples / BATCH_SIZE, LEARNING_RATE_DECAY)
+    train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+    with tf.control_dependencies([train_step, variable_averages_op]):
+        train_op = tf.no_op(name = 'train')
+
+    #初始化TensorFlow模型持久化类
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        
